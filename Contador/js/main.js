3d2ac840cce4ea6149c354d09935b9951c06ab83
yared17 <yared2017@gmail.com>
2021-09-11 00:29:50 -0500

    V-on:click

---
+++ Contador/js/main.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    data(){
+        return{
+        title: "Contador app - Vue",
+        count: 0,
+    };
+    },
+    methods: {
+        modCount(instruction="add", limit = 1){
+            if(instruction ==='dis')
+this.count -= limit;
+else
+this.count += limit;
+        },
+    },
+});
