9faaa268e4bef2e40ec9c7da802039c4f34a627e
yared17 <yared2017@gmail.com>
2021-09-10 21:18:10 -0500

    Segunda prueba

---
+++ 2vue/main.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            user:{
+            title: "Hola mundo",
+            name: "yared",
+            age: 26,
+            movies: ["spiderman" , "batman","superman"],
+            url: "https://www.google.com",
+            picture: "https://cdn.pixabay.com/photo/2020/07/21/16/24/landscape-5426755_960_720.jpg",
+        classValue: "good",
+        }
+    }
+    }
+})
